2f735079436625bd0e2847509ffc6fbd23418ed6
Egenie-mih <astra2826@gmail.com>
2017-08-21 01:30:14 +0300

    Наводит порядок, дополняет элементы

--- gulpfile.js
+++ gulpfile.js
@@ -28,20 +28,20 @@ gulp.task("style", function() {
             })
         ]))
         .pipe(csscomb())
-        .pipe(gulp.dest("build/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(minify())
         .pipe(rename("style.min.css"))
-        .pipe(gulp.dest("build/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(server.stream());
 });
 
 gulp.task("images", function() {
-    return gulp.src("build/img/**/*.{png, jpg, gif}")
+    return gulp.src("docs/img/**/*.{png, jpg, gif}")
         .pipe(imagemin([
             imagemin.optipng({optimizationLevel: 3}),
             imagemin.jpegtran({progressive: true})
         ]))
-        .pipe(gulp.dest("build/img"));
+        .pipe(gulp.dest("docs/img"));
 });
 
 gulp.task("symbols", function() {
@@ -103,9 +103,9 @@ gulp.task("copy", function () {
     ], {
       base: "."
     })
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("docs"));
 });
 
 gulp.task("clean", function() {
-  return del("build");
+  return del("docs");
 });
