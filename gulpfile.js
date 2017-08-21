a7070654f155e186afeb64d5831e95bf86c51627
Egenie-mih <astra2826@gmail.com>
2017-08-21 10:28:30 +0300

    Добавляет правки

--- gulpfile.js
+++ gulpfile.js
@@ -28,10 +28,10 @@ gulp.task("style", function() {
             })
         ]))
         .pipe(csscomb())
-        .pipe(gulp.dest("docs/css"))
+        .pipe(gulp.dest("./css"))
         .pipe(minify())
         .pipe(rename("style.min.css"))
-        .pipe(gulp.dest("docs/css"))
+        .pipe(gulp.dest("./css"))
         .pipe(server.stream());
 });
 
